34f9a733a839ac68b3803bc1d079844cd7159080
jembrown <jeremymbrown@gmail.com>
2018-12-09 22:44:34 -0600

    Comments added for in-class assignment

--- assignments/Inclassassignemntweek8.py
+++ assignments/Inclassassignemntweek8.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
+
 #asks the user to output the DNA sequence
-DNAseq=input ('Enter your DNA sequence here')
+DNAseq=input ('Enter your DNA sequence here: ')
+
 #convert DNA sequence into uppercase 
 DNAseq=DNAseq.upper()
 print (DNAseq)
+
 #first replace "T"s and "G"s with other letters to confusion
 DNAseq=DNAseq.replace("T","x")
 DNAseq=DNAseq.replace("G","y")
+
 #replace "A"s and "C"s to "T"s and "G"s.
 DNAseq=DNAseq.replace("A","T")
 DNAseq=DNAseq.replace("C","G")
+
 #replace "y" and "x" to "A"s and "C"s
-DNAseq=DNAseq.replace("x","A")
+DNAseq=DNAseq.replace("x","A")	# DB: Clever strategy!
 DNAseq=DNAseq.replace("y","C")
+
 #reverse the complementary strand formed above
 DNAseq=(DNAseq[::-1])
 print(DNAseq)
 
+# DB: Overall, looks good! I'd suggest adding spaces between logical
+#     blocks of code to improve readability. This is particularly true
+#     when you have comments.
